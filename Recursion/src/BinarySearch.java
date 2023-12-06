2d3696f582a61c99d14ce68dcb7f1df1898f5d9b
Thirunavukarasu M <mailme.thirunavukarasu@gmail.com>
2023-12-06 11:44:11 +0530

    Added Binary Search with recursion

--- Recursion/src/BinarySearch.java
+++ Recursion/src/BinarySearch.java
@@ -1,2 +1,20 @@
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(binarySearchWithRecursion(arr,500,0,arr.length - 1));
+    }
+    static int binarySearchWithRecursion(int[] arr, int target,int start, int end) {
+        if(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                return binarySearchWithRecursion(arr,target,start,mid-1);
+            } else {
+                return binarySearchWithRecursion(arr,target,mid + 1,end);
+            }
+        }
+
+        return -1;
+    }
 }
